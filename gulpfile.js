7cf6ce2b79a530d189bd4eb7a4da5bb4c8ee153d
zjy1988 <3104972476@qq.com>
2017-10-09 09:33:45 +0800

    status

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,20 @@ const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const minifyCss = require('gulp-minify-css');
 const notify = require('gulp-notify');
+const rename = require('gulp-rename');
 // 压缩js
 gulp.task('uglify',function(){
     gulp.src('js/*.js')
         .pipe(uglify())
+        .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('zip'))
         .pipe(notify({msg:'js压缩'}))
 })
 // 合并压缩后的js
 gulp.task('concat',function(){
     gulp.src('zip/*.js')
-        .pipe(concat('min.js'))
+        .pipe(concat('concat.js'))
+        .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('zip'))
         .pipe(notify({msg:'合并js'}))
 })
@@ -21,6 +24,8 @@ gulp.task('concat',function(){
 gulp.task('minify-css',function(){
     gulp.src('css/*.css')
         .pipe(minifyCss())
+        .pipe(rename({suffix:'.min'}))
         .pipe(gulp.dest('zip'))
         .pipe(notify({msg:'css压缩'}))
 })
+gulp.task('default',['uglify','concat','minify-css']);
